4fae68102816c79878129854871eab969f46042c
Ankur Dhakar <dhakar.ankur18@gmail.com>
2020-07-14 19:07:08 +0530

    view\

--- job1.groovy
+++ job1.groovy
@@ -46,3 +46,14 @@ fi
         ''')
     }
 }
+
+buildPipelineView('project-A') {
+    filterBuildQueue()
+    filterExecutors()
+    title('Project A CI Pipeline')
+    displayedBuilds(5)
+    selectedJob('project-A-compile')
+    alwaysAllowManualTrigger()
+    showPipelineParameters()
+    refreshFrequency(60)
+}
