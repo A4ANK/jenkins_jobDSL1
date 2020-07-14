93f8de4ed6525a08537850c7739d3d09e058fe68
Ankur Dhakar <dhakar.ankur18@gmail.com>
2020-07-14 20:14:50 +0530

    pass

--- job4.groovy
+++ job4.groovy
@@ -14,7 +14,7 @@ else
 	echo "Error code $statuscode"
     echo "triggering the job2 for creating resources for the deployment and mailing to the dev"
     sudo python3  /storage/mail_script.py
-    curl --user "admin:redhat" http://192.168.99.102:8080/job/Job1/build?token=secret
+    curl --user "admin:<password>" http://192.168.99.102:8080/job/Job1/build?token=secret
     exit 0
 fi
         ''')
