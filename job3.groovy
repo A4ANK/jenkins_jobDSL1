54a8792468287c548a114c2e615ce7e923392bdf
Ankur Dhakar <dhakar.ankur18@gmail.com>
2020-07-14 18:39:05 +0530

    git changes

--- job3.groovy
+++ job3.groovy
@@ -1,23 +1,19 @@
 job("Job3"){
     description("first job")
-
-    scm {
-        github('A4ANK/jenkins_jobDSL1.git', 'master')
-    }
     triggers {
             upstream('Job1', 'SUCCESS')
     }
     steps {
         shell ('''
-            statuscode=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.101:31000/index.html)
-            if [ $statuscode == '200' ]
-            then
-                echo "Webpage tested successfully"
-                exit 0
-            else
-                echo "Error code $statuscode"
-                exit 1
-            fi
+statuscode=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.101:31000/index.html)
+if [ $statuscode == '200' ]
+then
+	echo "Webpage tested successfully"
+    exit 0
+else
+	echo "Error code $statuscode"
+    exit 1
+fi
         ''')
     }
 }
